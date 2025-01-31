0949c5ca431e006a1c31b6d798e3bcbc74263129
Kenny Lewis <kl569@nau.edu>
2025-01-31 10:44:27 -0700

    Add handle functionality. Simply the CSS
    Removed most of the template CSS and simplified the classes

--- src/app/components/AddTodoInput.tsx
+++ src/app/components/AddTodoInput.tsx
@@ -1,18 +1,29 @@
 "use client";
-import { useState } from "react";
+import { FC, useState } from "react";
 
-const AddTodoInput = () => {
+export interface AddTodoInputProps {
+  onAdd: (label: string) => void;
+}
+
+const AddTodoInput: FC<AddTodoInputProps> = ({ onAdd }) => {
   const [input, setInput] = useState("");
 
+  const handleAddTodoFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onAdd(input);
+    setInput("");
+  };
+
   return (
-    <>
+    <form onSubmit={handleAddTodoFormSubmit}>
       <input
         type="text"
         placeholder="Add new item"
+        className="bg-neutral-50 border border-neutral-300 text-neutral-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-neutral-900 dark:border-neutral-700 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         value={input}
         onChange={(e) => setInput(e.currentTarget.value)}
       />
-    </>
+    </form>
   );
 };
 
--- src/app/components/TodoItem.tsx
+++ src/app/components/TodoItem.tsx
@@ -1,10 +1,28 @@
-const TodoItem = ({ todo }: {todo: Todo}) => {
-    return (
-        <div className="flex items-center gap-4">
-            <input type="checkbox" checked={todo.checked} />
-            <p className={todo.checked ? "line-through" : ""}>{todo.label}</p>
-        </div>
-    );
+import { FC } from "react";
+
+export interface TodoItemProps {
+  id: string;
+  label: string;
+  checked: boolean;
+  onChange: (id: string, checked: boolean) => void;
+}
+
+const TodoItem: FC<TodoItemProps> = ({
+  id,
+  label,
+  checked = false,
+  onChange,
+}) => {
+  return (
+    <div className="flex items-center gap-4">
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => onChange(id, e.target.checked)}
+      />
+      <p className={checked ? "line-through" : ""}>{label}</p>
+    </div>
+  );
 };
 
 export default TodoItem;
