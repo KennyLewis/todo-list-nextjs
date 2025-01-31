fe094181c030c878914f8f420c83895060091497
Kenny Lewis <kl569@nau.edu>
2025-01-31 10:45:05 -0700

    Add handle functionality. Simply the CSS (#2)
    Removed most of the template CSS and simplified the classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,57 @@
 "use client";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { v4 as uuid } from "uuid";
 import AddTodoInput from "./components/AddTodoInput";
 import TodoItem from "./components/TodoItem";
 
-
 export default function Home() {
+  const initialData: Todo[] = [
+    {
+      id: uuid(),
+      label: "Check email",
+      checked: true,
+    },
+    {
+      id: uuid(),
+      label: "Schedule meeting with team",
+      checked: false,
+    },
+    {
+      id: uuid(),
+      label: "Eat lunch",
+      checked: false,
+    },
+  ];
 
-const initialData: Todo[] = [
-  {
-    id: uuid(),
-    label: "Check email",
-    checked: true,
-  },
-  {
-    id: uuid(),
-    label: "Schedule meeting with team",
-    checked: false,
-  },
-  {
-    id: uuid(),
-    label: "Eat lunch",
-    checked: false,
-  },
-];
+  const [todos, setTodos] = useState<Todo[]>(initialData);
 
-const [todos, setTodos] = useState<Todo[]>(initialData);
+  const handleAddTodo = useCallback((label: string) => {
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: uuid(),
+        label,
+        checked: false,
+      },
+    ]);
+  }, []);
+
+  const handleChange = useCallback((id: string, checked: boolean) => {
+    setTodos((prevTodos) => {
+      const updatedTodos = prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, checked } : todo
+      );
+      return updatedTodos;
+    });
+  }, []);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Todo List</h1>
-        <AddTodoInput />
-        {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
+    <div className="grid justify-center content-center gap-4 p-4">
+      <h1 className="text-2xl font-bold text-center">Todo List</h1>
+      <AddTodoInput onAdd={handleAddTodo} />
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} {...todo} onChange={handleChange} />
+      ))}
     </div>
   );
 }
